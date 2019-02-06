c7922a6fe2db29b86e9b2d93d0904cd763359b28
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-02-06 22:50:05 +0800

    Format Code

--- test.php
+++ test.php
@@ -1,8 +1,8 @@
 <?php
-print($_SERVER['HTTP_USER_AGENT']);
-echo '<br>';
+  print($_SERVER['HTTP_USER_AGENT']);
+  echo '<br>';
 
-$ch = curl_init();
+  $ch = curl_init();
 
 /* Guide
     - $url = your target website
@@ -10,34 +10,33 @@ $ch = curl_init();
     - $agent = Fix for Current Device Info
 */
 
-$url = "google.com";
-$key = "";
-$agent = $_SERVER['HTTP_USER_AGENT'];
+  $url = "google.com";
+  $key = "";
+  $agent = $_SERVER['HTTP_USER_AGENT'];
 
-curl_setopt($ch, CURLOPT_URL, 'https://urlscan.io/api/v1/scan/');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"url\": \"$url\", \"public\": \"on\", \"useragent\": \"$agent\"}");
+  curl_setopt($ch, CURLOPT_URL, 'https://urlscan.io/api/v1/scan/');
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_POST, 1);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"url\": \"$url\", \"public\": \"on\", \"useragent\": \"$agent\"}");
 
-$headers = array();
-$headers[] = 'Content-Type: application/json';
-array_push($headers,"API-Key: " . $key);
+  $headers = array();
+  $headers[] = 'Content-Type: application/json';
+  array_push($headers,"API-Key: " . $key);
 
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
-$result = curl_exec($ch);
+  $result = curl_exec($ch);
 
+  $result_1 = json_decode($result, true);
+  echo '<br>';
+  print_r($result_1);
+  echo '<br>';
+  print($result_1['uuid']);
+  echo '<br>';
 
-$result_1 = json_decode($result, true);
-echo '<br>';
-print_r($result_1);
-echo '<br>';
-print($result_1['uuid']);
-echo '<br>';
-
-if (curl_errno($ch)) {
-    echo 'Error:' . curl_error($ch);
-}
-curl_close ($ch);
+  if (curl_errno($ch)) {
+      echo 'Error:' . curl_error($ch);
+  }
+  curl_close ($ch);
 
 ?>
